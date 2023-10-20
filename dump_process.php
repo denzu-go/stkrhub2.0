94a21fb73cf16be6539a754cb34374eff92fe199
denzu-go <73340375+denzu-go@users.noreply.github.com>
2023-10-20 13:30:43 +0800

    wlkajdf

--- dump_process.php
+++ dump_process.php
@@ -1,34 +1,350 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="zxx" class="no-js">
+
 <head>
-    <title>Select Option Example</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/icon.png">
+    <!-- Author Meta -->
+    <meta name="author" content="CodePixar">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <title>STKR HUB</title>
+
+    <!--CSS================================= -->
+    <link rel="stylesheet" href="css/linearicons.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/font-awesome.min.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/themify-icons.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/bootstrap.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/owl.carousel.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/nice-select.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/nouislider.min.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/ion.rangeSlider.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.css?<?php echo time(); ?>" />
+    <link rel="stylesheet" href="css/magnific-popup.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/main2.css?<?php echo time(); ?>">
+
+    <!-- scroll reveal -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+    <!-- sweetalert -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Include DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+
+
+
+
+    <style>
+        <?php include 'css/body.css'; ?>.header_area {
+            position: relative;
+        }
+
+        .swiper-slide {
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .image-banner-container {
+            overflow: hidden;
+            width: 100%;
+
+
+            position: relative;
+            padding-top: 45.25%;
+            /* 9/16 aspect ratio (16:9) */
+        }
+
+        .image-banner {
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+
+
+
+
+        .image-mini-container {
+            overflow: hidden;
+            width: 100%;
+
+
+            position: relative;
+            padding-top: 45.25%;
+            /* 9/16 aspect ratio (16:9) */
+        }
+
+        .image-mini {
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+
+
+
+        .iframe-container {
+            overflow: hidden;
+            width: 100%;
+
+
+            position: relative;
+            padding-top: 45.25%;
+            /* 9/16 aspect ratio (16:9) */
+        }
+
+        .iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+
+
+
+
+        @import url(https://fonts.googleapis.com/css?family=Raleway);
+
+        .content {
+            position: relative;
+        }
+
+        .content-overlay {
+            background: rgba(0, 0, 0, 0.7);
+            position: absolute;
+            opacity: 0;
+            -webkit-transition: all 0.4s ease-in-out 0s;
+            -moz-transition: all 0.4s ease-in-out 0s;
+            transition: all 0.4s ease-in-out 0s
+        }
+
+        .content:hover .content-overlay {
+            opacity: 1
+        }
+
+        img {
+            box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.1);
+            border-radius: 0px
+        }
+
+        .content-details {
+            position: absolute;
+            text-align: center;
+            padding-left: 1em;
+            padding-right: 1em;
+            width: 100%;
+            top: 50%;
+            left: 50%;
+            opacity: 0;
+            -webkit-transform: translate(-50%, -50%);
+            -moz-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            -webkit-transition: all 0.3s ease-in-out 0s;
+            -moz-transition: all 0.3s ease-in-out 0s;
+            transition: all 0.3s ease-in-out 0s
+        }
+
+        .content:hover .content-details {
+            top: 50%;
+            left: 50%;
+            opacity: 1
+        }
+
+        .content-details h3 {
+            color: #fff;
+            font-weight: 500;
+            letter-spacing: 0.15em;
+            margin-bottom: 0.5em;
+            text-transform: uppercase
+        }
+
+        .content-details p {
+            color: #fff;
+            font-size: 0.8em
+        }
+
+        .fadeIn-bottom {
+            top: 70%
+        }
+
+
+        .features-inner {
+            box-shadow: none !important;
+            padding: 40px 0;
+        }
+
+
+        /* card */
+        .product_card {
+
+            background-image: linear-gradient(to bottom, transparent 60%, #272a4e 100%);
+            border-radius: 10px;
+            display: flex;
+            padding: 1.7px;
+            transition: background-image 0.3s, transform 0.3s ease;
+            cursor: pointer;
+        }
+
+        .product_card:hover {
+            background-image: linear-gradient(to bottom, transparent 60%, #b660e8 100%);
+            transform: scale(1.03);
+        }
+
+        .product_card .card {
+            background: linear-gradient(to top, #272a4e 0%, #272a4e 25%);
+            border-radius: 10px;
+            width: 100%;
+        }
+
+        .product_card .card:hover {
+            background: linear-gradient(to top, #49265d 0%, #272a4e 20%);
+        }
+
+        <?php include 'css/header.css'; ?>
+    </style>
+
+
 </head>
+
 <body>
-    <label for="payment_method">Select Payment Method:</label>
-    <select name="payment_method" id="payment_method">
-        <option value="paypal">PayPal</option>
-        <option value="stkr_wallet">STKR Wallet</option>
-    </select>
+    <?php
+    include 'connection.php';
+    include 'html/page_header.php';
+    ?>
 
-    <div id="paypal_selected">Selected PayPal</div>
-    <div id="stkr_selected" style="display: none">Selected STKR Wallet</div>
 
+
+
+    <div class="container d-flex align-items-stretch">
+        <nav id="sidebar" class="img" style="background-image: url(images/bg_1.jpg);">
+            <div class="p-4">
+                <h1><a href="index.html" class="logo">Travel <span>Travel Agency</span></a></h1>
+                <ul class="list-unstyled components mb-5">
+                    <li class="active">
+                        <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-user mr-3"></span> About</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-plane mr-3"></span> Destination</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-sticky-note mr-3"></span> Blog</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-cogs mr-3"></span> Services</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-paper-plane mr-3"></span> Contacts</a>
+                    </li>
+                </ul>
+
+                <div class="mb-5">
+                    <h3 class="h6 mb-3">Subscribe for newsletter</h3>
+                    <form action="#" class="subscribe-form">
+                        <div class="form-group d-flex">
+                            <div class="icon"><span class="icon-paper-plane"></span></div>
+                            <input type="text" class="form-control" placeholder="Enter Email Address">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="footer">
+                    <p>
+                        Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+                    </p>
+                </div>
+
+            </div>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content" class="p-4 p-md-5 pt-5">
+            <h2 class="mb-4">Sidebar #06</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
+
+
+
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/nouislider.min.js"></script>
+    <script src="js/countdown.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!--gmaps Js-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+    <script src="js/gmaps.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Include DataTables JavaScript -->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+    <!-- sweetalert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Filepond JavaScript -->
+    <script src="https://unpkg.com/filepond@4.23.1/dist/filepond.min.js"></script>
+
+    <!-- Include DataTables JavaScript -->
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
+    <!-- scroll reveal js -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+
+
+
+    <!-- Initialize Swiper -->
     <script>
-        $(document).ready(function () {
-            $("#paypal_selected").show();
-            
-            $("#payment_method").change(function () {
-                var selectedOption = $(this).val();
-                if (selectedOption === 'paypal') {
-                    $("#paypal_selected").show();
-                    $("#stkr_selected").hide();
-                } else if (selectedOption === 'stkr_wallet') {
-                    $("#paypal_selected").hide();
-                    $("#stkr_selected").show();
-                }
-            });
+        $(document).ready(function() {
+
+
         });
     </script>
 </body>
+
 </html>
