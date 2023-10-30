920157018aaa831e3a64e368240964dbc83afc24
emcee0510 <113955507+emcee0510@users.noreply.github.com>
2023-10-30 15:09:24 +0800

    ayos na tab panel

--- add_custom_component.php
+++ add_custom_component.php
@@ -20,7 +20,6 @@ while ($fetchedGetGameInfo = $queryGetGameInfo->fetch_assoc()) {
     $is_built = $fetchedGetGameInfo['is_built'];
 }
 
-$category;
 ?>
 
 <!DOCTYPE html>
@@ -93,18 +92,18 @@ $category;
 
             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" id="pills-1-tab" data-toggle="pill" href="#pills-1" role="tab" aria-controls="pills-1" aria-selected="true">All</a>
+                    <a class="nav-link active" id="pills-0-tab" data-toggle="pill" href="#pills-0" role="tab" aria-controls="pills-0" aria-selected="true">All</a>
                 </li>
                 <?php
-                $sql = "SELECT DISTINCT category
-        FROM game_components";
+                $sql = "SELECT * 
+        FROM component_category";
                 $query = $conn->query($sql);
 
                 while ($row = $query->fetch_assoc()) {
-                    $category = $row['category'];
+                    $category = $row["category"];
 
                     echo '<li class="nav-item">
-            <a class="nav-link" id="pills-' . $category . '-tab" data-toggle="pill" href="#pills-' . $category . '" role="tab" aria-controls="pills-' . $category . '" aria-selected="false">' . $category . '</a>
+            <a class="nav-link" id="pills-' . $row['component_category_id'] . '-tab" data-toggle="pill" href="#pills-' . $row['component_category_id'] . '" role="tab" aria-controls="pills-' . $row['component_category_id'] . '" aria-selected="false">' . $category . '</a>
           </li>';
                 }
                 ?>
@@ -113,7 +112,7 @@ $category;
             </ul>
 
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-1" role="tabpanel" aria-labelledby="pills-1-tab">
+                <div class="tab-pane fade show active" id="pills-0" role="tabpanel" aria-labelledby="pills-0-tab">
                     <?php
                     $sql = "SELECT *
                    FROM game_components
@@ -153,45 +152,63 @@ $category;
                     ?>
                 </div>
 
-                <div class="tab-pane fade" id="pills-<?php echo $category; ?>" role="tabpanel" aria-labelledby="pills-<?php echo $category; ?>-tab">
-                    <?php
-                    $sql = "SELECT *
-                    FROM game_components
-                    LEFT JOIN component_category ON game_components.category COLLATE utf8mb4_unicode_ci = component_category.category COLLATE utf8mb4_unicode_ci
-                    WHERE game_components.category = '$category'"; // Make sure to include single quotes around $category
+                <?php
+$sql = "SELECT * FROM component_category";
+$query = $conn->query($sql);
 
-                    $query = $conn->query($sql);
+while ($row = $query->fetch_assoc()) {
+    $category = $row["category"];
 
-                    while ($row = $query->fetch_assoc()) {
-                        $component_id = $row["component_id"];
-                        $component_name = $row["component_name"];
-                        $description = $row["description"];
-                        $price = $row["price"];
-                        $has_colors = $row["has_colors"];
-                        $is_upload_only = $row["is_upload_only"];
-                        $size = $row["size"];
+    echo '<div class="tab-pane fade" id="pills-' . $row['component_category_id'] . '" role="tabpanel" aria-labelledby="pills-' . $row['component_category_id'] . '-tab">';
 
-                        $sqlA = "SELECT * FROM component_assets WHERE component_id = $component_id";
-                        $queryA = $conn->query($sqlA);
-                        while ($rowA = $queryA->fetch_assoc()) {
-                            $asset_id = $rowA["asset_id"];
-                            $asset_path = $rowA["asset_path"];
-                            $is_thumbnail = $rowA["is_thumbnail"];
-                        }
+    $category_id = $row['component_category_id']; // Store the category ID for the inner loop
 
-                        echo '
+    $sql = "SELECT *
+    FROM game_components
+    LEFT JOIN component_category ON game_components.category COLLATE utf8mb4_unicode_ci = component_category.category COLLATE utf8mb4_unicode_ci
+    WHERE component_category.component_category_id = $category_id";
+
+    $queryInner = $conn->query($sql); // Use a different variable for the inner query
+
+    while ($rowInner = $queryInner->fetch_assoc()) {
+        $component_id = $rowInner["component_id"];
+        $component_name = $rowInner["component_name"];
+        $description = $rowInner["description"];
+        $price = $rowInner["price"];
+        $has_colors = $rowInner["has_colors"];
+        $is_upload_only = $rowInner["is_upload_only"];
+        $size = $rowInner["size"];
+
+        $sqlA = "SELECT * FROM component_assets WHERE component_id = $component_id";
+        $queryA = $conn->query($sqlA);
+
+        while ($rowA = $queryA->fetch_assoc()) {
+            $asset_id = $rowA["asset_id"];
+            $asset_path = $rowA["asset_path"];
+            $is_thumbnail = $rowA["is_thumbnail"];
+        }
+
+        echo '
         <a href="game_component_details.php?game_id=' . $game_id . '&component_id=' . $component_id . '">
             <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="' . $asset_path . '" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">' . $component_name . '</h5>
+                    <h5 class "card-title">' . $component_name . '</h5>
                     <p class="card-text">' . $description . '</p>
                 </div>
             </div>
         </a>';
-                    }
-                    ?>
-                </div>
+    }
+
+    echo '</div>'; // Close the tab-pane div for the outer loop
+}
+?>
+
+
+
+
+
+
 
 
             </div>
