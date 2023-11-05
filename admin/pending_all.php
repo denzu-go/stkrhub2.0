ae9121edf0ef4fbb108445cd8dad94fa755f7f03
denzu-go <73340375+denzu-go@users.noreply.github.com>
2023-11-05 19:51:59 +0800

    sql

--- admin/pending_all.php
+++ admin/pending_all.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include 'connection.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +35,6 @@ include 'connection.php';
 
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
 
 </head>
 
@@ -111,21 +109,6 @@ include 'connection.php';
             </div>
         </div>
 
-
-        <?php
-        $zip = new ZipArchive();
-
-        if ($zip->open('newest.zip', ZipArchive::CREATE) === TRUE) {
-
-            $zip->addFile('../uploads/sample/sample2.txt', 'sample2.txt');
-            $zip->close();
-            // echo 'ZIP archive created successfully!';
-        } else {
-            // echo 'Failed to create ZIP archive.';
-        }
-
-        echo '<a href="newest.zip" download>Download newest ZIP Archive</a>';
-        ?>
 
 
         <div class="footer">
